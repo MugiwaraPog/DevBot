ec2f813529c3c10581dc8660b7e35515ab154f81
thiagocodex <thiago_codex@outlook.com.br>
2021-08-03 17:03:42 -0300

    Vídeo-aula 29, Deletando mensagens em massa [PARTE 3]

--- src/me/thiagocodex/devbot/commands/BulkDelete.java
+++ src/me/thiagocodex/devbot/commands/BulkDelete.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BulkDelete extends ListenerAdapter {
 
@@ -18,14 +19,26 @@ public class BulkDelete extends ListenerAdapter {
 
         String[] args = event.getMessage().getContentRaw().split(" ");
         if (args[0].equalsIgnoreCase(DevBot.prefixMap.get(event.getGuild().getId()) + "clear")) {
-            event.getChannel().deleteMessages(event.getChannel().getHistory().retrievePast(30).complete()).queue();
 
+            int num = Integer.parseInt(args[1]);
+
+            if (num <= 100 && num > 1) {
+                num = num >= 100 ? 99 : num;
+            } else if (num == 1) {
+                event.getChannel().sendMessage("Sério? Você não precisa deletar em massa 1 mensagem. Exclua-a").queue();
+                return;
+            } else {
+                event.getChannel().sendMessage("Você não pode apagar mais de 100 mensagens de uma só vez.").queue();
+                return;
+            }
+            event.getChannel().deleteMessages(event.getChannel().getHistory().retrievePast(num + 1)
+                    .complete().stream().filter(message -> !message.isPinned()).collect(Collectors.toList())).queue();
         }
 
         if (args[0].equalsIgnoreCase(DevBot.prefixMap.get(event.getGuild().getId()) + "clearall")) {
 
             if (!event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
-                event.getChannel().sendMessage("você não tem permissão para limpar este canal.").queue();
+                event.getChannel().sendMessage("Você não tem permissão para limpar este canal.").queue();
                 return;
             }
 
